8b6ddc2486c27fadd35d6d003b5ee6010cf57657
ziaochina <ziaochina@gmail.com>
2017-08-17 13:59:51 +0800

    zlj

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ import { config, start, componentFactory } from 'mk-meta-engine'
 import * as mkComponents from 'mk-component'
 import myConfig  from './config'
 
-import mk_app_voucher_education from './apps/apps/mk-app-voucher-education/index.js'
-import mk_app_voucher from './apps/index.js'
 import mk_app_bar_graph from './apps/mk-app-bar-graph/index.js'
 import mk_app_complex_table from './apps/mk-app-complex-table/index.js'
 import mk_app_editable_table from './apps/mk-app-editable-table/index.js'
@@ -18,11 +16,11 @@ import mk_app_root_about from './apps/mk-app-root/apps/mk-app-root-about/index.j
 import mk_app_root_helloWorld from './apps/mk-app-root/apps/mk-app-root-helloWorld/index.js'
 import mk_app_root from './apps/mk-app-root/index.js'
 import mk_app_tree_table from './apps/mk-app-tree-table/index.js'
+import mk_app_voucher_education from './apps/mk-app-voucher/apps/mk-app-voucher-education/index.js'
+import mk_app_voucher from './apps/mk-app-voucher/index.js'
 
 const apps = {
 		
-	[mk_app_voucher_education.name]: mk_app_voucher_education,	
-	[mk_app_voucher.name]: mk_app_voucher,	
 	[mk_app_bar_graph.name]: mk_app_bar_graph,	
 	[mk_app_complex_table.name]: mk_app_complex_table,	
 	[mk_app_editable_table.name]: mk_app_editable_table,	
@@ -36,7 +34,9 @@ const apps = {
 	[mk_app_root_about.name]: mk_app_root_about,	
 	[mk_app_root_helloWorld.name]: mk_app_root_helloWorld,	
 	[mk_app_root.name]: mk_app_root,	
-	[mk_app_tree_table.name]: mk_app_tree_table,
+	[mk_app_tree_table.name]: mk_app_tree_table,	
+	[mk_app_voucher_education.name]: mk_app_voucher_education,	
+	[mk_app_voucher.name]: mk_app_voucher,
 }
 
 apps.config = (options) => {
