916402bda0ee1423075f834c7fa220aabbe77be6
ziaochina <ziaochina@gmail.com>
2017-08-26 21:32:12 +0800

    zlj

--- apps/mk-app-portal/action.js
+++ apps/mk-app-portal/action.js
@@ -82,7 +82,7 @@ class action {
                 }
                 break;
             case 'github':
-                window.open('https://www.github.com/ziaochina/mk-app-portal')
+                window.open('https://www.github.com/ziaochina/mk-demo')
                 break;
             case 'gitter':
                 window.open('https://gitter.im/mk-js/mk-js?utm_source=share-link&utm_medium=link&utm_campaign=share-link')
