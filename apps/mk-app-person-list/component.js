6f1638610bd471e94a5b0c08c6f8be9bded170ca
ziaochina <ziaochina@gmail.com>
2017-09-28 15:45:33 +0800

    zlj

--- apps/mk-app-person-list/component.js
+++ apps/mk-app-person-list/component.js
@@ -5,7 +5,6 @@ import appInfo from './index'
 @wrapper(appInfo)
 export default class C extends Component {
 	render() {
-		console.log('list render')
 		return this.props.monkeyKing({ ...this.props, path: 'root' })
 	}
 }
